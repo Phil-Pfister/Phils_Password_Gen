3dbc7a2bab09d3e5e4acf7e9f43d22461af49783
Phil-Pfister <salshouse@gmail.com>
2023-03-08 19:31:14 -0600

    working javascript for password generation

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,58 @@
 // My code below
 function generatePassword() {
+  //answer prompts
   var specialCharacters = confirm("Would you like your password to include Special Characters?");
   var capitalLetters = confirm("Would you like your password to contain capital letters?");
   var numbers = confirm("Would you like your password to contain numbers?");
   var sizeOfPassword = prompt("How many characters would you like your password to contain? Enter a number between 8 and 18.");
-  var charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+  // variables for password
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  var num = "0123456789"
+  var symbols = "!#$%&()*+,-./:;<=>?@[]^_`{|}~";
+
+  // function to weed out variables
+  function charSet1() {
+        // all cases are true
+    if (specialCharacters && capitalLetters && numbers) {
+          return lower.concat(upper, num, symbols);
+          
+        // no numbers
+        } else if (specialCharacters && capitalLetters) {
+          return lower.concat(num, symbols);
+          
+        // no symbols 
+        } else if (capitalLetters && numbers) {
+          return lower.concat(upper, num);
+          
+          // no uppercase
+        } else if (specialCharacters && numbers) {
+          return lower.concat(symbols, num);
+          
+          // only symbols and lowercase
+        } else if (specialCharacters) {
+          return lower.concat(symbols);
+          
+          //only uppercase and lowercase
+        } else if (capitalLetters) {
+            return lower.concat(upper)
+          
+            // only lowercase and numbers
+        } else if (numbers) {
+          return lower.concat(num);
+
+          // only lowercase
+        } else {
+          return lower;
+          
+        }
+  }
+
+  // defines lengthens and repeats the character set
+  var charSetPrime = charSet1();
+  var charSet = charSetPrime.repeat(5);
+  
+  // converts string to an array and randomizes
   var charSetArray = charSet.split("");
   
   function shuffleArray(array) {
@@ -17,10 +65,13 @@ function generatePassword() {
   shuffleArray(charSetArray);
   
   var shuffle = charSetArray;
-  
+
+  // reduces the size of the array to the length inputted by user and 
+  // converts back to a string
   shuffle = charSetArray.slice(0,sizeOfPassword).toString();
   shuffle = shuffle.replaceAll(',', '');
   
+  // returns the new string to be used as the value of password
   return(shuffle);
 }
 
